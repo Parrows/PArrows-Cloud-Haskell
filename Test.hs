9b287be1c19e2f9e57e848b6c1721faef5462f87
Martin Braun <martinbraun123@aol.com>
2018-04-17 20:54:09 +0200

    wip on Testbed for Parallel Arrows in Cloud Haskell

--- Test.hs
+++ Test.hs
@@ -4,6 +4,7 @@
 {-# LANGUAGE TemplateHaskell     #-}
 {-# LANGUAGE GADTs     #-}
 {-# LANGUAGE KindSignatures     #-}
+{-# LANGUAGE Rank2Types     #-}
 
 import System.Environment (getArgs)
 import Control.Distributed.Process
@@ -19,8 +20,11 @@ import Data.IORef
 import System.IO.Unsafe
 
 import Control.Monad
+import Control.Monad.Fix
+import Data.Binary
+import Data.Typeable
 
-import Control.Concurrent
+import Control.Concurrent(forkIO, threadDelay)
 
 import Control.Concurrent.MVar
 
@@ -41,29 +45,52 @@ initialConf = do
   startedMVar <- newMVar False
   return State { workers = workersMVar, shutdown = shutdownMVar, started = startedMVar }
 
-class (NFData a) => Evaluatable a where
-  eval :: a -> Closure (Process a)
+class (Binary a, Typeable a, NFData a) => Evaluatable a where
+  evalTask :: (SendPort (SendPort a), SendPort a) -> Closure (Process ())
 
-tskBase :: NFData a => a -> Process a
-tskBase a = (rnf a) `seq` (return a)
+evalTaskBase :: (Binary a, Typeable a, NFData a) => 
+  (SendPort (SendPort a), SendPort a) -> Process ()
+evalTaskBase (inputPipe, output) = do
+  (sendMaster, rec) <- newChan
+  -- send the master the SendPort, that we
+  -- want to listen the other end on for the input
+  sendChan inputPipe sendMaster
+  a <- receiveChan rec
+  sendChan output (rnf a `seq` a)
 
-tsk :: Int -> Process Int
-tsk = tskBase
+evalTaskInt :: (SendPort (SendPort Int), SendPort Int) -> Process ()
+evalTaskInt = evalTaskBase
 
-$(remotable ['tsk])
+$(remotable ['evalTaskInt])
 
 instance Evaluatable Int where
-  eval = $(mkClosure 'tsk)
+  evalTask = $(mkClosure 'evalTaskInt)
 
 myRemoteTable :: RemoteTable
 myRemoteTable = Main.__remoteTable initRemoteTable
 
+forceSingle :: (Evaluatable a) => NodeId -> MVar a -> a -> Process ()
+forceSingle node out a = do
+  (inputSenderSender, inputSenderReceiver) <- newChan
+  (outputSender, outputReceiver) <- newChan
+
+  spawn node (evalTask (inputSenderSender, outputSender))
+
+  inputSender <- receiveChan inputSenderReceiver
+  sendChan inputSender a
+
+  forcedA <- receiveChan outputReceiver
+
+  liftIO $ putMVar out a
+
 {-
-tests :: NT.Transport -> IO ()
-tests transport = do
-  runProcess localNode (runTasksInPool mvar)
-  val <- takeMVar mvar
-  putStrLn $ show $ val
+
+force :: (Evaluatable a) => Conf -> MVar [a] -> [a] -> Process ()
+force conf out as = do
+  nodeIds <- return $ unsafePerformIO $ readMVar $ workers conf
+  let mvars = repeat (new)
+
+  return ()
   -}
 
 master :: Conf -> Backend -> [NodeId] -> Process ()
@@ -78,11 +105,20 @@ master conf backend slaves = do
           slaveProcesses <- findSlaves backend
           let slaveNodes = map processNodeId slaveProcesses
           liftIO $ do
-             modifyMVar_ (workers conf) (\old -> return slaveNodes)
-             modifyMVar_ (started conf) (\_ -> return True)
+              modifyMVar_ (workers conf) (\old -> return slaveNodes)
+              if (length slaveNodes) > 0 then
+                modifyMVar_ (started conf) (\_ -> return True)
+              else
+                return ()
 
--- processMain :: Conf -> Process ()
--- processMain
+waitUntil condition = fix $ \loop -> do
+  cond <- condition
+  if cond
+    then return ()
+    else threadDelay 100 >> loop
+
+hasSlaveNode :: Conf -> IO Bool
+hasSlaveNode conf = readMVar (started conf)
 
 main :: IO ()
 main = do
@@ -92,12 +128,18 @@ main = do
     ["master", host, port] -> do
       conf <- initialConf
       backend <- initializeBackend host port myRemoteTable
+
+      -- fork away the master node
       forkIO $ startMaster backend (master conf backend)
-      forever $ do
-        threadDelay 1000000
-        readMVar (workers conf) >>= print
-        -- putStrLn $ "Conf: " ++ show state
-      --
+
+      -- wait for startup
+      waitUntil (hasSlaveNode conf)
+
+      -- wait a bit
+      threadDelay 1000000
+      readMVar (workers conf) >>= print
+
+      -- TODO: actual computation here!
     ["slave", host, port] -> do
       backend <- initializeBackend host port myRemoteTable
       startSlave backend
